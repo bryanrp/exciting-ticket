2eaaa24398a3a2f101fda1718c8500375c5889ef
bryanrp <66724556+bryanrp@users.noreply.github.com>
2024-03-20 00:10:39 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Button from './components/Button';
+import Field from './components/Field';
 
-function App() {
-  const [count, setCount] = useState(0)
+const pages = [
+  'Page 1',
+  'Page 2',
+  'Page 3',
+  'Page 4',
+  'Page 5',
+  'Page 6',
+];
+
+export default function App() {
+  const [checkbox, setCheckbox] = useState(Array(pages.length).fill(false));
+  const [globalCheckbox, setGlobalCheckbox] = useState(false);
+
+  const toggleCheckbox = (idx: number) => {
+    const newCheckboxValues = [...checkbox];
+    newCheckboxValues[idx] = !newCheckboxValues[idx];
+    setCheckbox(newCheckboxValues);
+
+    const shouldGlobalChecked = newCheckboxValues.every((val) => val);
+    setGlobalCheckbox(shouldGlobalChecked);
+  }
+
+  const toggleGlobalCheckbox = () => {
+    setGlobalCheckbox(!globalCheckbox);
+    setCheckbox(Array(pages.length).fill(!globalCheckbox));
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="card">
+
+      <Field checked={globalCheckbox} toggleCheckbox={toggleGlobalCheckbox}>All pages</Field>
+
+      <hr className="line" />
+
+      <div className="scrollable-container">
+        {
+          pages.map((val, idx) => (
+            <Field
+              key={idx}
+              checked={checkbox[idx]}
+              toggleCheckbox={() => toggleCheckbox(idx)}
+            >
+              {val}
+            </Field>
+          ))
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <hr className="line" />
+
+      <Button>Done</Button>
+
+    </div>
   )
 }
-
-export default App
